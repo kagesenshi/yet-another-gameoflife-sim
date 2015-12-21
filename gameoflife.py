6fc091333a2cdea39edded17000b2392d44f5ce8
Izhar Firdaus <kagesenshi.87@gmail.com>
2015-12-22 01:38:41 +0800

    add todo and some docs

--- gameoflife.py
+++ gameoflife.py
@@ -109,12 +109,21 @@ class GameOfLife(object):
             for x, cell in enumerate(row):
                 v = self._get_cell(x, y)
                 if cell:
+
                     if v['live_neighbors'] < 2 or v['live_neighbors'] > 3:
+                        # Any live cell with fewer than two live neighbours 
+                        # dies, as if caused by under-population.
+                        # Any live cell with more than three live neighbours
+                        # dies, as if by over-population.
                         nrow.append(0)
                     else:
+                        # Any live cell with two or three live neighbours lives
+                        # on to the next generation.
                         nrow.append(1)
                 else:
                     if v['live_neighbors'] == 3:
+                        # Any dead cell with exactly three live neighbours
+                        # becomes a live cell, as if by reproduction.
                         nrow.append(1)
                     else:
                         nrow.append(0)
