8bb2576a9250195b111ad6a3441daba7e841b412
Izhar Firdaus <kagesenshi.87@gmail.com>
2016-01-05 03:31:40 +0800

    only init virtualenv if not yet exist

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 
-virtualenv venv
+if [ ! -e ./venv/bin/python ];then
+    virtualenv venv
+fi
 ./venv/bin/python bootstrap-buildout.py
 ./bin/buildout -vvv
 pushd dev/pysiphae/pysiphae/static
 bower install
 popd
+
